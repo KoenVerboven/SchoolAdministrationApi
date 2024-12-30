0d78033a6fa1444cea206c6b5a571263f06832d5
Verboven Koen <koenverboven@gmail.com>
2024-12-30 15:41:02 +0100

    updating info file

---
+++ SchoolAdministration/Migrations/20241230132236_AddStudyPlanAndStudyPlanPart.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolAdministration.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudyPlanAndStudyPlanPart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxNumberOfStudents",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "StudyPlans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudyPlans", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudyPlans_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudyPlanParts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    GoalDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudyObjectiveArchievedInProcent = table.Column<int>(type: "int", nullable: false),
+                    StudyPlanId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudyPlanParts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudyPlanParts_StudyPlans_StudyPlanId",
+                        column: x => x.StudyPlanId,
+                        principalTable: "StudyPlans",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudyPlanParts_StudyPlanId",
+                table: "StudyPlanParts",
+                column: "StudyPlanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudyPlans_StudentId",
+                table: "StudyPlans",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudyPlanParts");
+
+            migrationBuilder.DropTable(
+                name: "StudyPlans");
+
+            migrationBuilder.DropColumn(
+                name: "MaxNumberOfStudents",
+                table: "Courses");
+        }
+    }
+}
