0372ed44a083b325192e6d272545ad8a795a6155
Verboven Koen <koenverboven@gmail.com>
2025-01-12 17:30:01 +0100

    Adding StudyPlan Controllers

---
+++ SchoolAdministration/Controllers/StudyPlanController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolAdministration.Models;
+using SchoolAdministration.Repositories.Interfaces;
+
+namespace SchoolAdministration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudyPlanController : ControllerBase
+    {
+        private readonly IStudyPlanRepository _studyPlanRepository;
+        public StudyPlanController(IStudyPlanRepository studyPlanRepository )
+        {
+            _studyPlanRepository = studyPlanRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudyPlan>>> GetAllStudyPlans()
+        {
+            var allStudyPlans = await _studyPlanRepository.GetAllAsync();
+            return Ok(allStudyPlans);
+        }
+        
+    }
+}
---
+++ SchoolAdministration/Controllers/StudyPlanPartController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolAdministration.Models;
+using SchoolAdministration.Repositories.Interfaces;
+
+namespace SchoolAdministration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudyPlanPartController : ControllerBase
+    {
+        private readonly IStudyPlanPartRepository _studyPlanPartRepository;
+
+        public StudyPlanPartController(IStudyPlanPartRepository studyPlanPartRepository)
+        {
+            _studyPlanPartRepository = studyPlanPartRepository;
+        }
+
+        [HttpGet]
+        public async Task <ActionResult<IEnumerable<StudyPlanPart>>> GetAllStudyPlanParts()
+        {
+            var allStudyPlanParts = await _studyPlanPartRepository.GetAllAsync();
+            return Ok(allStudyPlanParts);
+        }
+
+    }
+}
