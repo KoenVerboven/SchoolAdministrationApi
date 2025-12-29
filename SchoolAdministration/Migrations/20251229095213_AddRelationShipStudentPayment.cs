0fb9591bc184615792e9b117a7900c2f35b4b5ce
Verboven Koen <koenverboven@gmail.com>
2025-12-29 10:58:53 +0100

    Migration relationship between Student and Payment

---
+++ SchoolAdministration/Migrations/20251229095213_AddRelationShipStudentPayment.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolAdministration.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationShipStudentPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ArticleId",
+                table: "Payments",
+                newName: "InvoiceId");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "PaymentMethodId",
+                table: "Payments",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_StudentId",
+                table: "Payments",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Students_StudentId",
+                table: "Payments",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Students_StudentId",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_StudentId",
+                table: "Payments");
+
+            migrationBuilder.RenameColumn(
+                name: "InvoiceId",
+                table: "Payments",
+                newName: "ArticleId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PaymentMethodId",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+        }
+    }
+}
