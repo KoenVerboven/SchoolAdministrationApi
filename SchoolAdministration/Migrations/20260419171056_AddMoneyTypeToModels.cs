de3ae6398b799f51bd8df6720235e54aa48415e1
Verboven Koen <koenverboven@gmail.com>
2026-04-19 19:15:01 +0200

    Migration AddMoneyType

---
+++ SchoolAdministration/Migrations/20260419171056_AddMoneyTypeToModels.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolAdministration.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoneyTypeToModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Payments",
+                type: "money",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAmount",
+                table: "Invoices",
+                type: "money",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "InvoiceDetailLines",
+                type: "money",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Payments",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "money");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAmount",
+                table: "Invoices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "money");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "InvoiceDetailLines",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "money");
+        }
+    }
+}
