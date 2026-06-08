8a5824924d06be7f796e53aabf3d3e15b294571c
Verboven Koen <koenverboven@gmail.com>
2026-06-08 20:14:30 +0200

    Add unittests for Course and Homework

---
+++ TestSchoolAdmin/CourseControllerTests.cs
@@ -0,0 +1,155 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SchoolAdministration.AutoMapper;
+using SchoolAdministration.Controllers;
+using SchoolAdministration.Models.Domain.Course;
+using SchoolAdministration.Models.Domain.Student;
+using SchoolAdministration.Models.DTO;
+using SchoolAdministration.Repositories.Interfaces;
+
+namespace TestSchoolAdmin;
+
+
+public class CourseControllerTests
+{
+
+    private readonly Mock<ICourseRepository>? _mockCourseRepo;
+    private readonly Mock<ILogger<CourseController>> _mockILogger;
+    private readonly MapperConfiguration? _mapperConfiguration;                  
+              
+
+
+    public CourseControllerTests()
+    {
+        _mockCourseRepo = new Mock<ICourseRepository>(MockBehavior.Default);
+       _mockILogger = new Mock<ILogger<CourseController>>(MockBehavior.Default);
+
+        var myProfile = new MappingConfig();
+        _mapperConfiguration = new MapperConfiguration(
+            cfg => cfg.AddProfile(myProfile), new LoggerFactory()
+        );
+    }
+
+
+    [Fact]
+    public async Task GetAllAync_ShallReturnTypeOK_ForCourseListContainItems()
+    {
+        //arrange
+        var mapper = new Mapper(_mapperConfiguration);
+        _mockCourseRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(CourseList());
+        var controller = new CourseController(_mockCourseRepo.Object, _mockILogger.Object, mapper);                                                                                                                     
+
+        //act
+        var actionResult = await controller.GetAllCoursers();
+        //assert
+        Assert.IsType<OkObjectResult>(actionResult.Result);
+        Assert.NotNull(actionResult);
+    }
+
+    [Fact]
+    public async Task GetAsynById_ShallReturnCourseWithId_ForExistingCourseWithId()
+    {
+        //arrange
+        var course = new Course()
+        {
+            Id = 1,
+            CourseName = "Electronica A2",
+            CourseCode = "E2",
+            StartDate = DateTime.Now,
+            EndDate = DateTime.Now,
+            CoursePrice = 10.99M,
+            Students = new List<Student>()
+        };
+
+        var courseDTO = new CourseDTO
+        {
+            Id = course.Id,
+            CourseName = course.CourseName,
+            CourseCode = course.CourseCode,
+            StartDate = course.StartDate,
+            EndDate = course.EndDate,
+            CoursePrice = course.CoursePrice,
+            Students = new List<Student>()
+        };
+
+        var mapper = new Mapper(_mapperConfiguration);
+        _mockCourseRepo.Setup(x => x.GetByIdAsync(1)).ReturnsAsync(course);
+        var controller = new CourseController(_mockCourseRepo.Object, _mockILogger.Object, mapper);
+
+        //act
+        var actionResult = await controller.GetCourseById(1);
+        //assert
+        var okObjectResult = actionResult.Result as OkObjectResult;
+        Assert.NotNull(okObjectResult);
+
+        var actual = okObjectResult.Value as CourseDTO;
+        Assert.NotNull(actual);
+        Assert.Equal(courseDTO.Id, actual.Id);
+        Assert.Equal(courseDTO.CourseName, actual.CourseName);
+        Assert.Equal(courseDTO.CourseCode, actual.CourseCode);
+        Assert.Equal(courseDTO.StartDate, actual.StartDate);
+        Assert.Equal(courseDTO.EndDate, actual.EndDate);
+        Assert.Equal(courseDTO.CoursePrice, actual.CoursePrice);
+        Assert.Equivalent(courseDTO, actual);
+    }
+
+    [Fact]
+    public async Task CreateCourse_AddCourseCorrectly_WhenNewCourseIsAdded()
+    {
+        //arrange
+        var course = new CourseCreateDTO()
+        {
+            CourseName = "Mathematics M2",
+            CourseCode = "M2",
+            StartDate = DateTime.Now,
+            EndDate = DateTime.Now,
+            CoursePrice = 15.99M
+        };
+
+        var courseCreateDTO = new CourseCreateDTO() 
+        {
+            CourseName = "Mathematics M2",
+            CourseCode = "M2",
+            StartDate = DateTime.Now,
+            EndDate = DateTime.Now,
+            CoursePrice = 15.99M
+        };
+
+        var mapper = new Mapper(_mapperConfiguration);
+        var controller = new CourseController(_mockCourseRepo.Object, _mockILogger.Object, mapper);
+
+        //act
+        var actionResult = await controller.CreateCourse(courseCreateDTO);
+
+        //assert
+        Assert.IsType<CreatedAtActionResult>(actionResult.Result);
+    }
+
+    private IEnumerable<Course> CourseList()
+    {
+        IEnumerable<Course> courseList = [
+            new Course()
+                {
+                    Id = 1,
+                   CourseName = "Electronica A2",
+                   CourseCode= "E2," ,
+                   StartDate= DateTime.Now,
+                   EndDate= DateTime.Now,
+                   CoursePrice= 10.99M
+                },
+                new Course()
+                {
+                    Id = 2,
+                   CourseName = "Mathemtics M2",
+                   CourseCode= "E2," ,
+                   StartDate= DateTime.Now,
+                   EndDate= DateTime.Now,
+                   CoursePrice= 15.99M
+                }
+            ]; ;
+        return courseList;
+    }
+
+}
--- TestSchoolAdmin/HomeWorkControllerTest.cs
+++ TestSchoolAdmin/HomeWorkControllerTest.cs
@@ -1,16 +1,155 @@
-namespace SchoolAdministrationTests;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SchoolAdministration.AutoMapper;
+using SchoolAdministration.Controllers;
+using SchoolAdministration.Models.Domain.HomeWork;
+using SchoolAdministration.Models.Domain.Student;
+using SchoolAdministration.Models.DTO;
+using SchoolAdministration.Repositories.Interfaces;
+
+namespace TestSchoolAdmin;
 
 public class HomeWorkControllerTest
 {
+    private readonly Mock<IHomeWorkRepository>? _mockHomeWorkRepo;
+    private readonly Mock<ILogger<HomeWorkController>> _mockILogger;
+    private readonly MapperConfiguration? _mapperConfiguration;
+
+
     public HomeWorkControllerTest()
     {
-        
+        _mockHomeWorkRepo = new Mock<IHomeWorkRepository>(MockBehavior.Default);
+        _mockILogger = new Mock<ILogger<HomeWorkController>>(MockBehavior.Default);
+
+        var myProfile = new MappingConfig();
+        _mapperConfiguration = new MapperConfiguration(
+            cfg => cfg.AddProfile(myProfile), new LoggerFactory()
+        );
     }
 
 
+    [Fact]
+    public async Task GetAllAync_ShallReturnTypeOK_ForCourseListContainItems()
+    {
+        //arrange
+        var mapper = new Mapper(_mapperConfiguration);
+        _mockHomeWorkRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(HomeWorkList());
+        var controller = new HomeWorkController(_mockHomeWorkRepo.Object, _mockILogger.Object, mapper);
+
+        //act
+        var actionResult = await controller.GetAllHomeWorks();
+        //assert
+        Assert.IsType<OkObjectResult>(actionResult.Result);
+        Assert.NotNull(actionResult);
+    }
 
     [Fact]
-    public void TestMethod1()
+    public async Task GetAsynById_ShallReturnHomeWorkWithId_ForExistingHomeWorkWithId()
     {
+        //arrange
+        var homeWork = new HomeWork()
+        {
+            Id = 1,
+            Name = "HomeWork 1",
+            DueDate = DateTime.Now,
+            TeacherId = 1,
+            HomeWorkDetailLines = new HomeWorkDetailLine[]
+                   {
+                       new()
+                       {
+                           Id = 1,
+                           HomeWorkAssignment = "HomeWork Detail Line 1",
+                           HomeWorkId = 1
+                       },
+                       new()
+                       {
+                           Id = 2,
+                           HomeWorkAssignment = "HomeWork Detail Line 2",
+                           HomeWorkId = 1
+                       }
+                   }
+
+        };
+
+        var homeWorkDTO = new HomeWorkDTO
+        {
+            Id = homeWork.Id,
+            Name = homeWork.Name,
+            DueDate = homeWork.DueDate,
+            TeacherId= homeWork.TeacherId,
+            HomeWorkDetailLines = homeWork.HomeWorkDetailLines
+        };
+
+        var mapper = new Mapper(_mapperConfiguration);
+        _mockHomeWorkRepo.Setup(x => x.GetByIdAsync(1)).ReturnsAsync(homeWork);
+        var controller = new HomeWorkController(_mockHomeWorkRepo.Object, _mockILogger.Object, mapper);
+
+        //act
+        var actionResult = await controller.GetHomeworkById(1);
+        //assert
+        var okObjectResult = actionResult.Result as OkObjectResult;
+        Assert.NotNull(okObjectResult);
+
+        var actual = okObjectResult.Value as HomeWorkDTO;
+        Assert.NotNull(actual);
+        Assert.Equal(homeWorkDTO.Id, actual.Id);
+        Assert.Equal(homeWorkDTO.Name, actual.Name);
+        Assert.Equal(homeWorkDTO.DueDate, actual.DueDate);
+        Assert.Equal(homeWorkDTO.TeacherId, actual.TeacherId);
+        Assert.Equivalent(homeWorkDTO, actual);
+    }
+
+    private IEnumerable<HomeWork> HomeWorkList()
+    {
+        IEnumerable<HomeWork> homeWorkList = [
+            new HomeWork()
+                {
+                   Id = 1,
+                   Name= "HomeWork 1",
+                   DueDate= DateTime.Now,
+                   TeacherId= 1,
+                   HomeWorkDetailLines = new HomeWorkDetailLine[]
+                   {
+                       new()
+                       {
+                           Id = 1,
+                           HomeWorkAssignment = "HomeWork Detail Line 1",
+                           HomeWorkId = 1
+                       },
+                       new()
+                       {
+                           Id = 2,
+                           HomeWorkAssignment = "HomeWork Detail Line 2",
+                           HomeWorkId = 1
+                       }
+                   }
+                },
+            new HomeWork()
+                {
+                   Id = 2,
+                    Name= "HomeWork 1",
+                   DueDate= DateTime.Now,
+                   TeacherId= 1,
+                   HomeWorkDetailLines = new HomeWorkDetailLine[]
+                   {
+                       new()
+                       {
+                           Id = 1,
+                           HomeWorkAssignment = "HomeWork Detail Line 1",
+                           HomeWorkId = 1
+                       },
+                       new()
+                       {
+                           Id = 2,
+                           HomeWorkAssignment = "HomeWork Detail Line 2",
+                           HomeWorkId = 1
+                       }
+                   }
+
+                }
+            ]; ;
+        return homeWorkList;
     }
 }
