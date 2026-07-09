348d2e9d196954baf04512e8d452cba16fbccff0
Verboven Koen <koenverboven@gmail.com>
2026-07-09 07:03:28 +0200

    Add Migration 20260709045439_AddAuditFieldsToCourseTeacherStudent

---
+++ SchoolAdministration/Migrations/20260709045439_AddAuditFieldsToCourseTeacherStudent.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolAdministration.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditFieldsToCourseTeacherStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "Teachers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Teachers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Teachers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedBy",
+                table: "Teachers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Students",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Students",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedBy",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "StudentDiplomas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "StudentDiplomas",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "StudentDiplomas",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedBy",
+                table: "StudentDiplomas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Courses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "Courses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedBy",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "StudentDiplomas");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "StudentDiplomas");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "StudentDiplomas");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "StudentDiplomas");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Courses");
+        }
+    }
+}
