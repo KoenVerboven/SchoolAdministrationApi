f1adad35849fdcda92d37db126e57e2cb2b9059b
Verboven Koen <koenverboven@gmail.com>
2026-08-04 13:51:41 +0200

    Add ParentTeacherMeeting Model

---
+++ SchoolAdministration/Models/Domain/Meeting/ParentTeacherMeeting.cs
@@ -0,0 +1,17 @@
+﻿using SchoolAdministration.Models.Domain.Student;
+
+namespace SchoolAdministration.Models.Domain.Meeting
+{
+    public class ParentTeacherMeeting
+    {
+        public int Id { get; set; }
+        public  int StudentId { get; set; }
+        public int TeacherId { get; set; }
+        public DateTime MeetingStartDate { get; set; }
+        public DateTime MeetingEndDate { get; set; }
+        public required string MeetingPlace { get; set; }
+        string MeetingNotes { get; set; } = string.Empty;
+        public int Status { get; set; }
+        public required ICollection<Parent> Parents { get; set; }
+    }
+}
