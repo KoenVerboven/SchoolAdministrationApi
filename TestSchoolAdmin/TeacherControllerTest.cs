dac2e39aab1f1dc0c9e6f0f35df695b11df1c58d
Verboven Koen <koenverboven@gmail.com>
2026-08-18 22:06:19 +0200

    Add unit tests for TeacherController scenarios
    Added tests for NotFound, BadRequest, create, update, and delete actions in TeacherControllerTest.cs. Each test sets up mocks and asserts expected controller results.

--- TestSchoolAdmin/TeacherControllerTest.cs
+++ TestSchoolAdmin/TeacherControllerTest.cs
@@ -109,7 +109,192 @@ namespace SchoolAdministrationTests
             var actual = model;
             Assert.Equal(teacherDTO.LastName, actual.LastName);
             Assert.Equivalent(teacherDTO, actual);
+        }
 
+
+        [Fact]
+        public async Task GetAsynById_ShallReturnNotFoundResult_WhenTeacherNotFound()
+        {
+            //arrange
+            var teacher = new Teacher()
+            {
+                Id = 13,
+                FirstName = "Piet1",
+                LastName = "Gevers",
+                DateOfBirth = new DateTime(2000, 06, 15),
+                Gender = 1,
+                Email = "Piet1@test.be",
+                Phone = "448389639",
+                HireDate = new DateTime(2020, 10, 10),
+                LeaveDate = null,
+                MaritalStatusId = 1,
+                TeacherAddresses = null,
+                TeacherPresences = null,
+                SchoolClasses = null
+            };
+
+            _mockTeacherRepo.Setup(x => x.GetAsynById(13)).ReturnsAsync(teacher);
+            var controller = new TeacherController(_mockTeacherRepo.Object, _mockILogger.Object);
+
+            //act
+            var actionResult = await controller.GetTeacherById(99);
+
+            //assert
+            var notFoundObjectResult = actionResult.Result as NotFoundResult;
+            Assert.NotNull(notFoundObjectResult);
+        }
+
+        [Fact]
+        public async Task GetAsynById_ShallReturnBadRequest_WhenTeacherIdEqualsZero()
+        {
+            //arrange
+            var teacher = new Teacher()
+            {
+                Id = 13,
+                FirstName = "Piet1",
+                LastName = "Gevers",
+                DateOfBirth = new DateTime(2000, 06, 15),
+                Gender = 1,
+                Email = "Piet1@test.be",
+                Phone = "448389639",
+                HireDate = new DateTime(2020, 10, 10),
+                LeaveDate = null,
+                MaritalStatusId = 1,
+                TeacherAddresses = null,
+                TeacherPresences = null,
+                SchoolClasses = null
+            };
+
+            _mockTeacherRepo.Setup(x => x.GetAsynById(1)).ReturnsAsync(teacher);
+            var controller = new TeacherController(_mockTeacherRepo.Object, _mockILogger.Object);
+                        
+            //act
+            var actionResult = await controller.GetTeacherById(0);
+
+            //assert
+            Assert.IsType<BadRequestResult>(actionResult.Result);
+        }
+
+        [Fact]
+        public async Task CreateTeacher_AddTeacherCorrectly_WhenNewTeacherIsAdded()
+        {
+            //arrange
+            var teacher = new Teacher()
+            {
+                Id = 13,
+                FirstName = "Piet1",
+                LastName = "Gevers",
+                DateOfBirth = new DateTime(2000, 06, 15),
+                Gender = 1,
+                Email = "Piet1@test.be",
+                Phone = "448389639",
+                HireDate = new DateTime(2020, 10, 10),
+                LeaveDate = null,
+                MaritalStatusId = 1,
+                TeacherAddresses = null,
+                TeacherPresences = null,
+                SchoolClasses = null
+            };
+
+            var newTeacher = new TeacherCreateDTO()
+            {
+                FirstName = "Edmond",
+                LastName = "Poels",
+                DateOfBirth = new DateTime(2000, 06, 15),
+                Gender = 1,
+                Email = "Edmond@test.be",
+                Phone = "44837881",
+                HireDate = new DateTime(2020, 10, 10),
+                LeaveDate = null,
+                MaritalStatusId = 1,
+            };
+
+            _mockTeacherRepo.Setup(x => x.GetAsynById(13)).ReturnsAsync(teacher);
+            var controller = new TeacherController(_mockTeacherRepo.Object, _mockILogger.Object);
+
+            //act
+            var actionResult = await controller.CreateTeacher(newTeacher);
+
+            //assert
+            Assert.IsType<CreatedAtActionResult>(actionResult.Result);
+        }
+
+        [Fact]
+        public async Task UpdateTeacherAsync_UpdateTeacherCorrectly_WhenIdIsEqualToTeacherUpdateDTOIdAndModelStateIsValid()
+        {
+            //arrange
+            var teacher = new Teacher()
+            {
+                Id = 1,
+                FirstName = "Koen",
+                LastName = "Verboven",
+                DateOfBirth = new DateTime(1999, 10, 10),
+                Gender = 1,
+                Email = "koen@test.be",
+                Phone = "448389639",
+                HireDate = new DateTime(2020, 10, 10),
+                LeaveDate = null,
+                MaritalStatusId = 1,
+                TeacherAddresses = null,
+                TeacherPresences = null,
+                SchoolClasses = null
+            };
+
+            var updatedTeacher = new TeacherUpdateDTO()
+            {
+                Id = 1,
+                FirstName = "Pieter",
+                LastName = "Verboven",
+                DateOfBirth = new DateTime(1999, 10, 10),
+                Gender = 1,
+                Email = "koen@test.be",
+                Phone = "448389639",
+                HireDate = new DateTime(2020, 10, 10),
+                LeaveDate = null,
+                MaritalStatusId = 1,
+            };
+
+            _mockTeacherRepo.Setup(x => x.GetAsynById(1)).ReturnsAsync(teacher);
+            var controller = new TeacherController(_mockTeacherRepo.Object, _mockILogger.Object);
+
+            //act
+            var actionResult = await controller.UpdateTeacherAsync(1, updatedTeacher);
+
+            //assert
+            var okObjectResult = actionResult.Result as CreatedAtActionResult;
+            Assert.IsType<CreatedAtActionResult>(actionResult.Result);
+
+            var actual = okObjectResult!.Value as Teacher;
+            Assert.NotNull(actual);
+            Assert.Equal(updatedTeacher.Id, actual.Id);
+            Assert.Equal(updatedTeacher.FirstName, actual.FirstName);
+            Assert.Equivalent(updatedTeacher, actual);
+        }
+
+        [Fact]
+        public async Task DeleteTeacherById_DeleteCorrectly_WhenValidIdIsGiven()
+        {
+            //arrange
+            var teacher = new Teacher()
+            {
+                Id = 1,
+                FirstName = "Koen",
+                LastName = "Verboven",
+                DateOfBirth = new DateTime(1999, 10, 10),
+                Gender = 1,
+                Email = "koen@test.be",
+                Phone = "448389639",
+            };
+
+            _mockTeacherRepo.Setup(x => x.GetAsynById(1)).ReturnsAsync(teacher);
+            var controller = new TeacherController(_mockTeacherRepo.Object, _mockILogger.Object);
+
+            //act
+            var actionResult = await controller.DeleteTeacherById(1);
+
+            //assert
+            var okObjectResult = actionResult as NoContentResult;
+            Assert.IsType<NoContentResult>(actionResult);
         }
 
     }
